23f4322c6b0c2d681f26bc2c9975e90e1c9e7d32
rick427 <rchard@yahoo.com>
2021-05-17 11:50:31 +0100

    Added owneer policy

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ae10636806339d671aae837678c5bedf'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4d710ed2-b9c1-4a5e-b743-9ae21fd80471'
+};
